acd8ddbd3f291a0b081d146797236565a4dcb8c5
Matthew Hollick <22219356+matthew-hollick@users.noreply.github.com>
2021-10-21 18:17:05 +0100

    Now almost a complete metrics stack
    I'm using Haggar running on host os to drive metrics
    Not sure why I can not use port 2003 for otel-in :-/
    Most importantly, export from Redpanda to carbon-relay-ng is shockly
    slow. Maybe as low as 100 events a second. I thought this might be due
    to a bad interaction between otel and carbon-clickhouse so I shimmed in
    carbon-relay-ng but the performance is about the same..

--- init-db.sh
+++ init-db.sh
@@ -2,8 +2,36 @@
 set -e
 
 echo 'Hello!'
-clickhouse-client --query='DROP DATABASE IF EXISTS graphite'
-clickhouse-client --query='CREATE DATABASE graphite'
-clickhouse-client --database=graphite --query='CREATE TABLE graphite.graphite_tree (`Date` Date, `Level` UInt32, `Path` String, `Deleted` UInt8, `Version` UInt32) ENGINE = MergeTree() ORDER BY (Level, Path)'
-clickhouse-client --database=graphite --query='CREATE TABLE graphite.graphite (`Path` String, `Value` Float64, `Time` UInt32, `Date` Date, `Timestamp` UInt32) ENGINE = MergeTree() PARTITION BY toMonday(Date) ORDER BY (Path, Time)'
-clickhouse-client --database=graphite --query='SHOW TABLES'
+
+clickhouse client -n <<-EOSQL
+    DROP DATABASE IF EXISTS graphite;
+    CREATE DATABASE graphite;
+    CREATE TABLE IF NOT EXISTS graphite.graphite_reverse (
+      Path String,  
+      Value Float64,  
+      Time UInt32,  
+      Date Date,  
+      Timestamp UInt32
+    ) ENGINE = GraphiteMergeTree('graphite_rollup')
+    PARTITION BY toYYYYMM(Date)
+    ORDER BY (Path, Time);
+    
+    CREATE TABLE IF NOT EXISTS graphite.graphite_index (
+      Date Date,
+      Level UInt32,
+      Path String,
+      Version UInt32
+    ) ENGINE = ReplacingMergeTree(Version)
+    PARTITION BY toYYYYMM(Date)
+    ORDER BY (Level, Path, Date);
+
+    CREATE TABLE IF NOT EXISTS graphite.graphite_tagged (
+      Date Date,
+      Tag1 String,
+      Path String,
+      Tags Array(String),
+      Version UInt32
+    ) ENGINE = ReplacingMergeTree(Version)
+    PARTITION BY toYYYYMM(Date)
+    ORDER BY (Tag1, Path, Date);
+EOSQL
